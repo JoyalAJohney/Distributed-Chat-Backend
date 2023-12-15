d0de7726b427e5d60156cf7eb6b4895ce2dfe88f
Joyal A Johney <joyalajohny@gmail.com>
2023-12-15 17:14:44 +0530

    updated script file

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const serverUrl = 'ws://localhost:8080/ws/chat';  // Update with your WebSocket server URL
+const serverUrl = 'ws://54.251.26.199:8080/ws/chat';  // Update with your WebSocket server URL
 let socket;
 let roomName = '';
 
